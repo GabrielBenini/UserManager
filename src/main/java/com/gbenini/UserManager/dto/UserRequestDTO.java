1b06cbb56c84f0de88213b7deadfdab4a8fb4c0b
Gabriel <gabriel.benini2@gmail.com>
2025-11-03 14:48:22 -0300

    Criadas as DTOs para User

---
+++ src/main/java/com/gbenini/UserManager/dto/UserRequestDTO.java
@@ -0,0 +1,37 @@
+package com.gbenini.UserManager.dto;
+
+import com.gbenini.UserManager.model.entity.User;
+import com.gbenini.UserManager.model.enumerate.UserRole;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+
+import java.time.LocalDate;
+
+public record UserRequestDTO(
+
+        @NotBlank(message = "Name cannot be blank")
+        String name,
+        @NotBlank(message = "Email cannot be blank")
+        String email,
+        @NotBlank(message = "Password cannot be blank")
+        String password,
+        @NotNull(message = "Birth date cannot be null")
+        LocalDate birthDate,
+        @NotNull(message = "Role cannot be null")
+        UserRole role) {
+
+    public User toEntity() {
+
+        User user = new User();
+
+        user.setName(this.name);
+        user.setEmail(this.email);
+        user.setPassword(this.password);
+        user.setBirthDate(this.birthDate);
+        user.setRole(this.role);
+
+        return user;
+
+    }
+
+}
---
+++ src/main/java/com/gbenini/UserManager/dto/UserResponseDTO.java
@@ -0,0 +1,24 @@
+package com.gbenini.UserManager.dto;
+
+import com.gbenini.UserManager.model.entity.User;
+import com.gbenini.UserManager.model.enumerate.UserRole;
+
+import java.time.LocalDate;
+
+public record UserResponseDTO(Long userId, String name, String email, String password, LocalDate birthDate, UserRole role) {
+
+    private static UserResponseDTO fromEntity(User user){
+
+        return new UserResponseDTO(
+                user.getUserId(),
+                user.getName(),
+                user.getEmail(),
+                user.getPassword(),
+                user.getBirthDate(),
+                user.getRole()
+                
+        );
+
+    }
+
+}
