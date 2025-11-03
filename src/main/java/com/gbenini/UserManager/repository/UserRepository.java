9adc06cf1f7a5643f453362b08d7517b0b79e3e3
Gabriel <gabriel.benini2@gmail.com>
2025-11-03 15:47:02 -0300

    Criados metodos de busca na repository

--- src/main/java/com/gbenini/UserManager/repository/UserRepository.java
+++ src/main/java/com/gbenini/UserManager/repository/UserRepository.java
@@ -1,7 +1,24 @@
 package com.gbenini.UserManager.repository;
 
 import com.gbenini.UserManager.model.entity.User;
+import com.gbenini.UserManager.model.enumerate.UserRole;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.time.LocalDate;
+import java.util.List;
+
 public interface UserRepository extends JpaRepository<User, Long> {
+
+    List<User> findByName(String name);
+
+    List<User> findByEmail(String email);
+
+    List<User> findByPassword(String password);
+
+    List<User> findByBirthDate(LocalDate birthDate);
+
+    List<User> findByRole(UserRole role);
+
+
+
 }
